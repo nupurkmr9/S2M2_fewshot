7774272d5635c2ab960d3530d868905b3fe71acf
nupurkmr9 <nupurkmr9@gmail.com>
2020-10-24 01:42:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,3 +3,4 @@ data_dir = {}
 data_dir['cifar']             = './filelists/cifar/' 
 data_dir['CUB']             = './filelists/CUB/' 
 data_dir['miniImagenet']    = './filelists/miniImagenet/' 
+data_dir['tieredImagenet']    = './filelists/tieredImagenet/' 
